4c10196a56ea97acd13e916e11904287d260b7de
Tbrecke01 <tylerbrecke@yahoo.com>
2022-02-17 16:30:08 -0800

    Adding PyPoll.py file

---
+++ InClassWork/InclassTest.py
@@ -0,0 +1,13 @@
+name = "Tyler Brecke"
+country = "USA"
+age = 26
+hourly_wage = 26
+satisfied = True
+daily_wage = hourly_wage*8
+daily_wage
+print(name)
+print(country)
+print(age)
+print(hourly_wage)
+print(f"Hello my name is {name}. I am {age} and I make {hourly_wage} per hour.")
+print("Hello my name is {name}. I am {age} and I make {hourly_wage} per hour.")
